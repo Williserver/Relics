9464f35a2bc67b8191c65639ed974d8e4b82ba04
Will Morris <morris.william.work@gmail.com>
2025-05-12 14:57:51 -0400

    Initial commit

---
+++ src/main/net/williserver/relics/RelicsPlugin.kt
@@ -0,0 +1,14 @@
+package net.williserver.relics
+
+import org.bukkit.plugin.java.JavaPlugin
+
+/**
+ * RelicsPlugin, a plugin for tracking special items.
+ *
+ * @author Willmo3
+ */
+class RelicsPlugin: JavaPlugin() {
+    override fun onEnable() {
+        println("[RELICS] Relics plugin enabled!")
+    }
+}
